e33105428fb9d9518c30371d801cfe73672ff4b0
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2022-07-07 00:31:40 +0900

    modify dbconnection

--- database/database_connection.js
+++ database/database_connection.js
@@ -3,7 +3,15 @@ const mongoose = require("mongoose");
 const databaseConnection = () => {
     const dbAddress = "";
 
-    mongoose.connect(dbAddress, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(dbAddress, { 
+            poolSize: 10,
+            authSource: "",
+            user: "",
+            pass: "", 
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(() => console.log("MongoDB Connected"))
         .catch((err) => console.log(err));
 
